5f34118c25946f27b51fe3fe7bc5cf15f730f660
Filipe <filipe.almeida.18@gmail.com>
2022-10-24 01:48:15 +0100

    adapted template matching pre processing

--- hearts/hearts.py
+++ hearts/hearts.py
@@ -61,23 +61,3 @@ class Hearts:
             return winner
         else:
             return -1
-
-
-
-h = Hearts()
-print(h.play_trick([Deck.C2, Deck.C3, Deck.C4, Deck.C5]))
-print(h.play_trick([Deck.C6, Deck.C7, Deck.C8, Deck.C9]))
-print(h.play_trick([Deck.C10, Deck.CJ, Deck.CQ, Deck.CK]))
-print(h.play_trick([Deck.CA, Deck.D2, Deck.D3, Deck.D4]))
-print(h.play_trick([Deck.D5, Deck.D6, Deck.D7, Deck.D8]))
-print(h.play_trick([Deck.D9, Deck.D10, Deck.DJ, Deck.DQ]))
-print(h.play_trick([Deck.DK, Deck.DA, Deck.H2, Deck.H3]))
-print(h.play_trick([Deck.H4, Deck.H5, Deck.H6, Deck.H7]))
-print(h.play_trick([Deck.H8, Deck.H9, Deck.H10, Deck.HJ]))
-print(h.play_trick([Deck.HQ, Deck.HK, Deck.HA, Deck.S2]))
-print(h.play_trick([Deck.S3, Deck.S4, Deck.S5, Deck.S6]))
-print(h.play_trick([Deck.S7, Deck.S8, Deck.S9, Deck.S10]))
-print(h.play_trick([Deck.SJ, Deck.SQ, Deck.SK, Deck.SA]))
-
-print(h.player_cards)
-print(h.check_winner())
